d40e80f1c800c76c4f6c9c14534195031db55fbc
David Ralph <ohlookitsderpy@protonmail.com>
2019-03-17 10:05:13 +0000

    Update compile.js

--- compile.js
+++ compile.js
@@ -1,7 +1,7 @@
 const compile = [
     'tsc source/typescript.ts --outDir build',
     'gcc -o build/c source/c.c',
-    '-main source/Main -lib haxicord -x bin/bot.n'
+    'haxe -main source/Main -lib haxicord -x bin/bot.n'
 ]
 
 const exec = require('child_process').exec
