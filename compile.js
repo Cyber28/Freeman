6eaa2a9d2bc7b4308ccb6da6ca56bb8b67d87088
August (Chris) <augu@voided.pw>
2019-03-18 06:59:15 -0700

    C# language

--- compile.js
+++ compile.js
@@ -1,7 +1,8 @@
 const compile = [
     'tsc source/typescript.ts --outDir build',
     'gcc -o build/c source/c.c',
-    'cd source && haxe -main Main -x ../build/haxe.n'
+    'cd source && haxe -main Main -x ../build/haxe.n',
+    'cd source && dotnet build Program.cs'
 ]
 
 const exec = require('child_process').exec
